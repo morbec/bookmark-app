f01432c8298c51879888af6eec213cd6fd4f584d
Fabio Morbec <morbecfm@gmail.com>
2019-05-31 23:07:23 +0200

    Revert App.jsx to class until I learn hooks/Context properly.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/jsx-boolean-value */
 /* eslint-disable react/no-unused-state */
-import React, { useState, useEffect } from 'react'
+import React, { Component } from 'react'
 import { Router, Switch, Route } from 'react-router-dom'
 import { createBrowserHistory } from 'history'
 
@@ -17,47 +17,54 @@ import Home from './components/Home'
 
 const browserHistory = createBrowserHistory()
 
-const App = () => {
-  const [ userLoggedIn, setUserLoggedIn ] = useState(null)
-  // eslint-disable-next-line no-unused-vars
-  const [ bookmark, setBookmark ] = useState(null)
+class App extends Component {
+  state = {
+    userLoggedIn: null,
+    bookmark: null
+  }
 
-  const getUser = () => {
-    loggedIn().then((user) => {
-      setUser(user)
+  componentDidMount() {
+    this.getUser()
+  }
+
+  setUser = (user) => {
+    this.setState({
+      userLoggedIn: user
     })
   }
 
-  const setUser = (user) => {
-    setUserLoggedIn(user)
+  getUser = () => {
+    loggedIn().then((user) => {
+      this.setState({
+        userLoggedIn: user
+      })
+    })
   }
 
-  useEffect(() => {
-    getUser()
-  })
-
-  const saveUrl = (bookmark) => {
-    setBookmark(bookmark)
+  saveUrl = (bookmark) => {
+    this.setState({ bookmark })
   }
 
-  return (
-    <div className='App'>
-      <Router history={browserHistory}>
-        <AppNavbar setUser={setUser} userLoggedIn={userLoggedIn} />
-        <Switch>
-          <Route exact path='/' component={Home} />
-          <Route exact path='/sign-up' render={(props) => <SignUp {...props} setUser={setUser} />} />
-          <Route exact path='/login' render={(props) => <BALogin {...props} setUser={setUser} />} />
-          <Route
-            exact
-            path='/bookmarks'
-            render={(props) => <BookmarksList {...props} userLoggedIn={userLoggedIn} />}
-          />
-        </Switch>
-        <AppBottomBar saveUrl={saveUrl} userLoggedIn={userLoggedIn} />
-      </Router>
-    </div>
-  )
+  render() {
+    return (
+      <div className='App'>
+        <Router history={browserHistory}>
+          <AppNavbar setUser={this.setUser} userLoggedIn={this.state.userLoggedIn} />
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/sign-up' render={(props) => <SignUp {...props} setUser={this.setUser} />} />
+            <Route exact path='/login' render={(props) => <BALogin {...props} setUser={this.setUser} />} />
+            <Route
+              exact
+              path='/bookmarks'
+              render={(props) => <BookmarksList {...props} userLoggedIn={this.state.userLoggedIn} />}
+            />
+          </Switch>
+          <AppBottomBar saveUrl={this.saveUrl} userLoggedIn={this.state.userLoggedIn} />
+        </Router>
+      </div>
+    )
+  }
 }
 
 export default App
